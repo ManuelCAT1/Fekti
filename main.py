74b2558cb4e3608f5dac8c418024316027062dd5
ManuelCAT1 <126294597+ManuelCAT1@users.noreply.github.com>
2024-02-03 01:33:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ app.register_blueprint(views, name='my_views')
 app.config['UPLOAD_FOLDER'] = '/photos/'
 
 
+@app.template_filter('custom_b64encode')
+def custom_b64encode_filter(value):
+    if not isinstance(value, str):
+        value = str(value)
+    return base64.b64encode(value.encode('utf-8')).decode('utf-8')
+
 
 
 if __name__ == '__main__':
