1a78459671a32940dc32e62fea092bcef9a4563a
ManuelCAT1 <126294597+ManuelCAT1@users.noreply.github.com>
2024-02-03 12:50:45 +0100

    Update deletePhoto.py

--- deletePhoto.py
+++ deletePhoto.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Photo  # Import your Photo class here
+from models import Photo  # Import your Photo class here
 
 # Replace 'YourDatabaseURL' with the actual database URL
 engine = create_engine('instance/database.db')
